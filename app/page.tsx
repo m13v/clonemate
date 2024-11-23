ee34c54c666e6055a86e5e1adcaf43b5889d1716
matthew-heartful <matthew.heartful@gmail.com>
2024-11-23 15:46:17 -0800

    entry point

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { MyAssistant } from "@/components/MyAssistant";
 
 export default function Home() {
   return (
-    <main className="h-dvh">
+    <main className="h-dvh flex flex-col">
       <MyAssistant />
     </main>
   );
